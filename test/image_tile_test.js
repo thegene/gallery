d29d36d96736c6d77ce2759adede5f3b1d5a35e2
Eugene Westbrook <eugenewestbrook@gmail.com>
2016-04-04 17:12:18 -0500

    Adds failing test for second tile

--- test/image_tile_test.js
+++ test/image_tile_test.js
@@ -4,22 +4,43 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import ImageTile from '../app/image_tile';
 
-var subject;
+jsdom();
+
+var render_tile = function(imageUrl){
+  return TestUtils.renderIntoDocument(
+    <ImageTile imageUrl={imageUrl} />
+  );
+}
+
+var image_in_tile = function(tile){
+  return TestUtils.findRenderedDOMComponentWithTag(tile, 'img');
+}
 
 context('Given a rendered image tile with an image url', function(){
-  jsdom();
+  var firstTile;
 
   before(function(){
-    var imageUrl = 'foo.com';
-
-    subject = TestUtils.renderIntoDocument(
-      <ImageTile imageUrl={imageUrl} />
-    );
+    firstTile = render_tile('foo.com');
   });
 
   it('has an image tag with the provided url', function(){
-    expect(TestUtils.findRenderedDOMComponentWithTag(subject, 'img').getAttribute('src'))
-      .toBe('foo.com');
+    expect(
+      image_in_tile(firstTile).getAttribute('src')
+    ).toBe('foo.com');
+  });
+
+  context('given a second tile with a different image url', function(){
+    var secondTile;
+
+    before(function(){
+      secondTile = render_tile('bar.com');
+    });
+
+    it('has an image tag with the new url', function(){
+      expect(
+        image_in_tile(secondTile).getAttribute('src')
+      ).toBe('bar.com');
+    });
   });
 });
 
