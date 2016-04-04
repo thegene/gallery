f977eca79c558f9a67608d5c2d14c17321ea1d43
Eugene Westbrook <eugenewestbrook@gmail.com>
2016-04-03 20:12:04 -0500

    A lot of garbage to get a test to work with jsx

---
+++ app/image_tile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var ImageTile = React.createClass({
+  render: function(){
+    return (
+      <div className="ImageTile">
+        <img src="foo.com" />
+      </div>
+    );
+  }
+});
+
+export default ImageTile;
--- test/image_tile_test.js
+++ test/image_tile_test.js
@@ -1,7 +1,10 @@
-var jsdom = require('mocha-jsdom');
-var expect = require('expect');
-var React = require('react');
-var TestUtils = require('react-addons-test-utils');
+import jsdom from 'mocha-jsdom';
+import expect from 'expect';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import ImageTile from '../app/image_tile';
+
+var subject;
 
 context('Given a rendered image tile with an image url', function(){
   jsdom();
@@ -9,13 +12,13 @@ context('Given a rendered image tile with an image url', function(){
   before(function(){
     var imageUrl = 'foo.com';
 
-    var ImageTile = TestUtils.renderIntoDocument(
-      React.createElement('div', { imageUrl: imageUrl })
+    subject = TestUtils.renderIntoDocument(
+      <ImageTile imageUrl={imageUrl} />
     );
   });
 
   it('has an image tag with the provided url', function(){
-    expect(TestUtils.findRenderedDomComponentWithTag(ImageTile, 'img').getAttribute('src'))
+    expect(TestUtils.findRenderedDOMComponentWithTag(subject, 'img').getAttribute('src'))
       .toBe('foo.com');
   });
 });
