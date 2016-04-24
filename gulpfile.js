4e0715b412109840417d9a776fd7dd99bc1fef90
Eugene Westbrook <eugenewestbrook@gmail.com>
2016-04-24 12:19:41 -0500

    finally getting selenium to exit

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,10 @@ var babel = require('gulp-babel');
 require('babel-core/register');
 var selenium = require('selenium-standalone');
 var webdriver = require('gulp-webdriver');
+var runSequence = require('run-sequence');
 
-gulp.task('build', function(done){
-  gulp.src('*.js', { cwd: 'app/' })
+gulp.task('build', function(){
+  return gulp.src('*.js', { cwd: 'app/' })
     .pipe(webpack({
       module: {
         loaders:[{
@@ -39,17 +40,20 @@ gulp.task('test', function(){
 });
 
 //https://semaphoreci.com/community/tutorials/setting-up-an-end-to-end-testing-workflow-with-gulp-mocha-and-webdriverio
-gulp.task('e2e', ['build:development', 'e2e:run'], function(){
+gulp.task('e2e', function(done){
+  runSequence('build:development', 'e2e:run', 'e2e:cleanup');
+});
+
+gulp.task('e2e:cleanup', function(done){
   seleniumServer.kill();
 });
 
-gulp.task('e2e:run', ['selenium'], function(done){
-  wdioConf = {};
-  gulp.src('wdio.conf.js')
+gulp.task('e2e:run', ['selenium'], function(){
+  return gulp.src('wdio.conf.js')
     .pipe(webdriver())
     .on('error', function(){
       seleniumServer.kill();
-      done();
+      process.exit(1);
     });
 });
 
@@ -68,7 +72,7 @@ gulp.task('selenium', function(done){
 });
 
 gulp.task('build:development', ['clean', 'build'], function(done){
-  gulp.src('**', { cwd: 'development' })
+  return gulp.src('**', { cwd: 'development' })
     .pipe(gulp.dest('dist'));
 });
 
